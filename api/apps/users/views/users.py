1e6cc8b586c845c29727002cd70210bd70da261d
Mau Munguia <maumg1196@gmail.com>
2021-01-08 22:35:26 -0600

    Add Login URLs and Views

---
+++ api/apps/users/views/users.py
@@ -0,0 +1,26 @@
+"""Users views."""
+
+# Django
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import views as auth_views
+from django.urls import reverse, reverse_lazy
+from django.views.generic import (
+	FormView,
+	DetailView,
+	UpdateView,
+)
+
+# Models
+from apps.users.models import User
+
+
+class LoginView(auth_views.LoginView):
+    """Login view."""
+
+    template_name = 'users/login.html'
+
+
+class LogoutView(LoginRequiredMixin, auth_views.LogoutView):
+    """Logout view."""
+
+    template_name = 'users/logged_out.html'
--- api/config/settings.py
+++ api/config/settings.py
@@ -135,3 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# LOGIN URLs
+LOGIN_URL = '/'
+LOGIN_REDIRECT_URL = '/hits/'
+LOGOUT_REDIRECT_URL = LOGIN_URL
